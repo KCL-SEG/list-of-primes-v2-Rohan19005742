707fb42d27a68b41beee69c4a5ae3f48e0d75ec8
Rohan19005742 <60757115+Rohan19005742@users.noreply.github.com>
2022-10-13 16:41:19 +0100

    13/10/2022
    Completed task

--- primes.py
+++ primes.py
@@ -1,6 +1,23 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+"""List of prime numbers generator."""
+"""ENTER YOUR SOLUTION HERE!"""
+
 def primes(number_of_primes):
+    if (number_of_primes <= 0):
+        print("ValueError")
+        return
     list = []
+    number = 2
+    while (len(list) != number_of_primes):
+        if (isPrime(number)):
+            list.append(number)
+        number+=1
     return list
+
+def isPrime(number):
+    for x in range(2,int(number/2)+1):
+        if (number % x == 0):
+            return False
+    return True
